137d247d72a05179e315a49f48218ecf71c16a7d
Eric <ev18322@gmail.com>
2026-02-03 18:54:18 -0500

    rename terragrunt folder

--- terragrunt/modules/ecs_cluster/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  type        = string
-  description = "Name of the ECS cluster"
-}
-
-variable "ecs_task_definition_family" {
-  type        = string
-  description = "Family name for the ECS task definition"
-}
--- terragrunt/website_ecr/main.tf
+++
@@ -1,8 +0,0 @@
-module "ecr_repo" {
-  source = "../modules/ecr"
-  name   = local.repo_name
-}
-
-locals {
-  repo_name = "garden-website-repo"
-}
