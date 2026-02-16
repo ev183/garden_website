91eeecfe54acf794f00fb3cd28f26e6a41590816
Eric <ev18322@gmail.com>
2026-02-16 11:12:58 -0500

    add lifecycle policy for ECR repo

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,3 +1,42 @@
 resource "aws_ecr_repository" "this" {
   name = var.name
 }
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.this.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Archive images not pulled in 90 days",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "sinceImagePulled",
+        "countUnit": "days",
+        "countNumber": 90
+      },
+      "action": {
+        "type": "transition",
+        "targetStorageClass": "archive"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Delete images archived for more than 365 days",
+      "selection": {
+        "tagStatus": "any",
+        "storageClass": "archive",
+        "countType": "sinceImageTransitioned",
+        "countUnit": "days",
+        "countNumber": 365
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
